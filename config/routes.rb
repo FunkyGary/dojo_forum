490ab97554f76e5b60bce7803f8144de647e12f7
gary80212@gmail.com <gary80212@gmail.com>
2018-10-26 01:18:29 +0800

    admin categories

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'articles#index'
   resources :articles
+  resources :categories   # 請加入此行
   namespace :admin do
     resources :articles
+    resources :categories   # 請加入此行
     root 'articles#index'
   end
 end
