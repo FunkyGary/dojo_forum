97b41ddea562b19d82f52143b358f7e36d3ad191
gary80212@gmail.com <gary80212@gmail.com>
2018-10-25 23:34:39 +0800

    link category and restaurant model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
   validates_presence_of :title
   mount_uploader :image, PhotoUploader
+  belongs_to :category
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.destroy_all
 User.create(email: "admin@example.com", password: "12345678", role:'admin')
 
 puts 'admin user create'
@@ -21,3 +20,16 @@ Article.destroy_all
 end
 puts "have created fake articles"
 puts "now you have #{Article.count} articles data"
+
+Category.destroy_all
+
+category_list = [
+  { name: "Cat" },
+  { name: "Dog" },
+  { name: "Hamster" },
+]
+
+category_list.each do |category|
+  Category.create( name: category[:name] )
+end
+puts "Category created!"
