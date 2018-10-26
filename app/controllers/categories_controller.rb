fd4244f549efbb4fac8c64b8f16eba6249e6604c
gary80212@gmail.com <gary80212@gmail.com>
2018-10-27 01:25:56 +0800

    kalamari

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,11 @@
 class CategoriesController < ApplicationController
   def index
     @categories = Category.all
+    @articles = Article.page(params[:page]).per(9)
+  end
+  def show
+    @categories = Category.all
+    @category = Category.find(params[:id])
+    @articles = @category.articles.page(params[:page]).per(9)
   end
 end
