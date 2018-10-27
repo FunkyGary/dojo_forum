29ecc14a98f037f84e2f77eafbadd52bb3f9a55a
gary80212@gmail.com <gary80212@gmail.com>
2018-10-27 15:08:55 +0800

    comment delete

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,16 @@ class CommentsController < ApplicationController
     redirect_to article_path(@article)
   end
 
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = Comment.find(params[:id])
+
+    if current_user.comments
+      @comment.destroy
+      redirect_to article_path(@article)
+    end
+  end
+
   private
 
   def comment_params
