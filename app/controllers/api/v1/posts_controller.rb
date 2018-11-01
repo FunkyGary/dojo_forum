2cd8b5be80a1d5ef91eb16b441cf8b17b670db1b
gary80212@gmail.com <gary80212@gmail.com>
2018-11-02 00:58:52 +0800

    add create action for photo api

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,42 @@
+class Api::V1::PostsController < ApiController
+  def index
+    @posts = Article.all
+    render json: {
+      data: @posts
+    }
+  end
+  def show
+    @post = Article.find_by(id: params[:id])
+    if !@post
+      render json: {
+        message: "Can't find the post!",
+        status: 400
+      }
+    else
+      render json: {
+        title: @post.title,
+        date: @post.created_at,
+        description: @post.description
+      }
+    end
+  end
+  def create
+    @post = Article.new(post_params)
+    if @post.save
+      render json: {
+        message: "Post created successfully!",
+        result: @post
+      }
+    else
+      render json: {
+        errors: @post.errors
+      }
+    end
+  end
+
+  private
+
+  def post_params
+    params.permit(:title, :date, :description, :file_location)
+  end
+end
